d63c6065efcfdcdd6532d66aed5cadff8c826474
Elesmer <paelesmer@gmail.com>
2019-03-09 18:41:17 +0200

    Add all

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var gulp         = require('gulp'), // Подключаем Gulp
 		cache        = require('gulp-cache'), // Подключаем библиотеку кеширования
 		autoprefixer = require('gulp-autoprefixer'),// Подключаем библиотеку для автоматического добавления префиксов
 		uncss        = require('gulp-uncss'),
-		smartgrid 	 = require('smart-grid'),
+		// smartgrid 	 = require('smart-grid'),
 		gcmq 	 			 = require('gulp-group-css-media-queries');
 
 gulp.task('sass', function(){ // Создаем таск Sass
@@ -104,44 +104,44 @@ gulp.task('build', ['clean', 'img', 'sass', 'scripts', 'gulp-group-css-media-que
 gulp.task('clear', function (callback) {
 	return cache.clearAll();
 })
- 
-/* It's principal settings in smart grid project */
-var settings = {
-    outputStyle: 'sass', /* less || scss || sass || styl */
-    columns: 12, /* number of grid columns */
-    offset: '30px', /* gutter width px || % */
-    container: {
-        maxWidth: '1200px', /* max-width оn very large screen */
-        fields: '30px' /* side fields */
-    },
-    breakPoints: {
-        lg: {
-            width: '1100px', /* -> @media (max-width: 1100px) */
-            fields: '30px' /* side fields */
-        },
-        md: {
-            width: '960px',
-            fields: '15px'
-        },
-        sm: {
-            width: '780px',
-            fields: '15px'
-        },
-        xs: {
-            width: '560px',
-            fields: '15px'
-        }
-        /* 
-        We can create any quantity of break points.
- 
-        some_name: {
-            some_width: 'Npx',
-            some_offset: 'N(px|%)'
-        }
-        */
-    }
-};
- 
-smartgrid('./app/sass', settings);
+
+// /* It's principal settings in smart grid project */
+// var settings = {
+//     outputStyle: 'sass', /* less || scss || sass || styl */
+//     columns: 12, /* number of grid columns */
+//     offset: '30px', /* gutter width px || % */
+//     container: {
+//         maxWidth: '1200px', /* max-width оn very large screen */
+//         fields: '30px' /* side fields */
+//     },
+//     breakPoints: {
+//         lg: {
+//             width: '1100px', /* -> @media (max-width: 1100px) */
+//             fields: '30px' /* side fields */
+//         },
+//         md: {
+//             width: '960px',
+//             fields: '15px'
+//         },
+//         sm: {
+//             width: '780px',
+//             fields: '15px'
+//         },
+//         xs: {
+//             width: '560px',
+//             fields: '15px'
+//         }
+//         /*
+//         We can create any quantity of break points.
+//
+//         some_name: {
+//             some_width: 'Npx',
+//             some_offset: 'N(px|%)'
+//         }
+//         */
+//     }
+// };
+//
+// smartgrid('./app/sass', settings);
 
 gulp.task('default', ['watch']);
